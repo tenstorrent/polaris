c1c94ab72e83395dd4a5a18d8971e77edacb5b25
Shreeniwas N Sapre <ssapre@tenstorrent.com>
2025-04-16 11:33:53 +0530

    Refactor Run Config Model
    * Add newly added polaris command line knobs as attributes to
      RunCfgModel
    * Attribute names of RunCfgModel made consistent with the corresponding
      polaris CLI knobs.

--- ttsim/config/runcfgmodel.py
+++ ttsim/config/runcfgmodel.py
@@ -8,48 +8,39 @@ from pydantic import BaseModel, Field, ValidationInfo, field_validator
 type TYPE_SWEEP = Literal['all', 'first-parent']
 type TYPE_RUN = Literal['inference', 'training']
 type TYPE_LOGLEVEL = Literal['critical', 'error', 'warning', 'info', 'debug']
+type TYPE_OUTPUTFORMAT = Literal['json', 'yaml', 'pickle', 'none']
 type TYPE_frequency = Tuple[int, int, int]
 type TYPE_batchsize = Tuple[int, int, int]
 
-class PolarisRunConfig(BaseModel, extra='forbid'):
-    title: str = Field(description='Human readable itle for the run')
-    output: str = Field(
-        description='This value is considered to be a Jinja2 template, and expanded as one, with the variables that '
-        'Kochab automatically defines prior to each run. Hence, the value can use the complete expressive power of '
-        'Jinja2 templates to construct the output directory names. '
-        '(TBD: provide pointer to the list of variables defined by Kochab that can be used in the template)'
-    )
-    study: str = Field(
-        description='Study name'
-    )
-    wlspec: str = Field(
-        description='Workload Specification yaml file'
-    )
-    archspec: str = Field(
-        description='Architecture Specification yaml file'
-    )
-    wlmapspec: str = Field(
-        description='Workload To Architecture Mapping Specification yaml file'
-    )
 
-    filterapi: Optional[str] = Field(
-        default=None,
-        description='APIs to be considered for the run'
+class PolarisRunConfig(BaseModel, extra='forbid'):
+    # Title is used only in the run config, it is not a command line attribute of polaris
+    title: str = Field(description='Human readable itle for the run')
+
+    # Attributes corresponding to polaris command line options
+    odir: str = Field(
+        description='This value is considered to be a Jinja2 template, and expanded as one, with the variables that '
+        'polproj automatically defines prior to each run. Hence, the value can use the complete expressive power of '
+        'Jinja2 templates to construct the output directory names. '
+        '(TBD: provide pointer to the list of variables defined by polproj that can be used in the template)'
     )
+    study: str = Field(description='Study name')
+    wlspec: str = Field(description='Workload Specification yaml file')
+    archspec: str = Field(description='Architecture Specification yaml file')
+    wlmapspec: str = Field(description='Workload To Architecture Mapping Specification yaml file')
+
+    filterapi: Optional[str] = Field(default=None, description='APIs to be considered for the run')
 
     filterwl: Optional[str] = Field(
-        default=None,
-        description='use only workloads specified in filterwl (comma sep list)'
+        default=None, description='use only workloads specified in filterwl (comma sep list)'
     )
 
     filterwli: Optional[str] = Field(
-        default=None,
-        description='use only workload instances specified in filterwli (comma sep list)'
+        default=None, description='use only workload instances specified in filterwli (comma sep list)'
     )
 
     filterarch: Optional[str] = Field(
-        default=None,
-        description='use only architectures specified in filterarch (comma sep list)'
+        default=None, description='use only architectures specified in filterarch (comma sep list)'
     )
 
     filterrun: Optional[TYPE_RUN] = Field(
@@ -57,14 +48,9 @@ class PolarisRunConfig(BaseModel, extra='forbid'):
         description='Filter either inference or training runs',
     )
 
-    filter: Optional[str] = Field(
-        default=None,
-        description='A regular expression to filter, occurring anywhere in the command'
-    )
-
     frequency: Optional[TYPE_frequency] = Field(
-        default=None,
-        description='frequency (in MHz) range specification (arith-seq)')
+        default=None, description='frequency (in MHz) range specification (arith-seq)'
+    )
 
     @field_validator('frequency')
     def validate_frequency(cls, v: TYPE_frequency, info: ValidationInfo) -> TYPE_frequency:
@@ -96,20 +82,31 @@ class PolarisRunConfig(BaseModel, extra='forbid'):
 
     dump_ttsim_onnx: Optional[bool] = Field(default=False, description='Dump ONNX graph for TTSIM Workload')
 
-    ignorefails: Optional[bool] = Field(default=False,
-                                        description='If true, run will continue even if some command(s) fails')
+    ignorefails: Optional[bool] = Field(
+        default=False, description='If true, run will continue even if some command(s) fails'
+    )
 
-    loglevel: Optional[TYPE_LOGLEVEL] = Field(
+    log_level: Optional[TYPE_LOGLEVEL] = Field(
         default='info', description='Logging level (error,warning,info,debug) for the run'
     )
 
-    githash: str | None = Field(
-        default=None, description='Particular git commit'
-    )
+    # githash attribute does not correspond to a polaris command line option
+    githash: str | None = Field(default=None, description='Particular git commit')
+
+    # Attributes corresponding to polaris command line options
+    enable_memalloc: Optional[bool] = Field(default=False, description='Enable memory allocation stats')
+
+    enable_cprofile: Optional[bool] = Field(default=False, description='Enable cProfiler stats')
+
+    outputformat: str = Field(default='json', description='Output format (json, yaml, pickle, none) for the run')
+
+    dumpstatscsv: Optional[bool] = Field(default=False, description='Dump stats in csv format')
+
+    # Attributes for use by Polaris system only
 
     saved_copy: Optional[bool] = Field(
         default=False,
-        description='Only for internal use. This attribute should be true for yaml copies saved by polaris system only'
+        description='Only for internal use. This attribute should be true for yaml copies saved by polaris system only',
     )
 
 
