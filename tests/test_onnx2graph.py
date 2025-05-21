1e6fbef2baaa57814f0a14f746199c21a06b1415
Shreeniwas N Sapre <ssapre@tenstorrent.com>
2025-05-21 22:09:29 +0530

    Update copyright text

--- tests/test_onnx2graph.py
+++ tests/test_onnx2graph.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# SPDX-FileCopyrightText: (C) 2025 Tenstorrent
+# SPDX-FileCopyrightText: (C) 2025 Tenstorrent AI ULC
 # SPDX-License-Identifier: Apache-2.0
 from ttsim.front.onnx.onnx2nx import onnx2graph
 
