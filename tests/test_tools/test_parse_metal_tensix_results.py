94aa6e9d4eb2ac61bb807395bbc089a1a742e7d1
Shreeniwas N Sapre <ssapre@tenstorrent.com>
2025-07-08 16:10:18 +0530

    Skip tests for secondary tools from checkin tests

--- tests/test_tools/test_parse_metal_tensix_results.py
+++ tests/test_tools/test_parse_metal_tensix_results.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
 # SPDX-FileCopyrightText: (C) 2025 Tenstorrent AI ULC
 # SPDX-License-Identifier: Apache-2.0
+import pytest
 from tools.parse_metal_tensix_results import parse_metal_tensix_results
 
-
+@pytest.mark.tools_secondary
 def test_parse_metal_tensix_results(tmp_path_factory):
     """
     Test the parsing of metal tensix performance numbers.
